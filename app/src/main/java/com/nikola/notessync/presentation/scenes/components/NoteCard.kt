bce905d42bbefc039f1f81150a722c8b8d1cd786
nikolahadzic <nikola.hadzic@hotmail.com>
2023-06-27 11:09:12 +0200

    Fix shadow, selection color and bolded title

--- app/src/main/java/com/nikola/notessync/presentation/scenes/components/NoteCard.kt
+++ app/src/main/java/com/nikola/notessync/presentation/scenes/components/NoteCard.kt
@@ -2,27 +2,48 @@ package com.nikola.notessync.presentation.scenes.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.nikola.notessync.domain.model.Note
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun NoteCard(note: Note, clicked: () -> Unit, longClicked: () -> Unit, selected: Boolean = false) {
+    var alpha by remember {
+        mutableStateOf(1f)
+    }
+    if (selected) {
+        alpha = 0.3f
+    } else {
+        alpha = 1f
+    }
     Card(
         modifier = Modifier
-            .aspectRatio(1f)
             .padding(8.dp)
+            .aspectRatio(1f)
+            .alpha(alpha),
+        colors = CardDefaults.cardColors(
+            containerColor = Note.noteColors[note.noteColor],
+            contentColor = Note.fontColors[note.fontColor]
+        )
+    ) {
+        Column(modifier = Modifier
+            .fillMaxSize()
             .combinedClickable(
                 onClick = {
                     clicked()
@@ -30,18 +51,12 @@ fun NoteCard(note: Note, clicked: () -> Unit, longClicked: () -> Unit, selected:
                 onLongClick = {
                     longClicked()
                 }
-            ),
-        colors = CardDefaults.cardColors(
-            containerColor = Note.noteColors[note.noteColor],
-            contentColor = Note.fontColors[note.fontColor]
-        )
-    ) {
-        if (selected) {
-            Icon(imageVector = Icons.Default.Delete, contentDescription = "Note selected for delete")
-        } else {
+            )) {
             Text(
                 modifier = Modifier
-                    .padding(8.dp), text = note.title
+                    .padding(8.dp),
+                text = note.title,
+                fontWeight = FontWeight.Bold
             )
 
             Text(
@@ -49,6 +64,5 @@ fun NoteCard(note: Note, clicked: () -> Unit, longClicked: () -> Unit, selected:
                 text = note.content
             )
         }
-
     }
 }
